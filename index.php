2d7493a02c98ba52462c38aaf2081f1623a5a1d2
Hassan Maudarbocus <HM1014@live.mdx.ac.uk>
2022-02-12 21:13:03 +0400

    removed unnecessry html files and updated php

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 include "functions.php";
 outHeader(["css/shoppingsite.css"]);
-include "html/home.html";
+?>
+
+    <!-- hero section -->
+    <header class="hero-section">
+    <div class="content">
+        <img src="images/header.png" class="hero-img">
+            <p class="sub-heading"></p>
+    </div>
+</header>
+
+<?php
 include "html/footer.html";
 
 ?>
